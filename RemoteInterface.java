acd2402122907f15370647aa00e821bcc9935859
Karine Bezerra Furtado <88294505+karinefurtado@users.noreply.github.com>
2023-12-22 08:34:23 -0300

    Delete RemoteInterface.java

--- RemoteInterface.java
+++
@@ -1,8 +0,0 @@
-package com.uern.rpcdistribuidos;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface RemoteInterface extends Remote {
-    String buscar(String nome) throws RemoteException;
-}
